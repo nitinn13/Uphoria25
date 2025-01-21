c30e1fc8e587867d81c1811e705b5d7be9b68f54
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2025-01-21 15:36:47 +0530

    Restored changes to App.jsx and Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,11 +26,11 @@ const router = createBrowserRouter([
 
 const App = () => {
   return (
-<<<<<<< HEAD
+
     <div className="w-[100vw] min-h-screen bg-uphoria bg-no-repeat bg-cover bg-center flex flex-col items-center font-ibm-plex-condensed">
-=======
-    <div className="w-screen min-h-screen bg-uphoria bg-no-repeat bg-cover bg-center flex flex-col items-center font-ibm-plex-condensed">
->>>>>>> 3b7a2b125ac876aeaf9d448a5ab4fdfc77908eba
+
+
+
 
       <RouterProvider router={router} />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,14 @@
 import React, { useState } from 'react';
 
 const Navbar = () => {
-<<<<<<< HEAD
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-=======
-  return (
-    <div className='flex flex-row justify-between items-center gap-[20vw]'>
-      <div>
-        <img src="logo1.svg" alt="TimesLogo" />
-      </div>
-      <div className="border-2 border-[#E6C373] rounded-3xl bg-[rgba(25,25,112,0.4)] text-white hover:border-gray-400 mx-4 my-3">
-        <ul className="flex flex-wrap justify-around items-center gap-7 mx-5 my-3">
-          {['Events', 'Timeline', 'Gallery',  'Contact', 'Sponsors'].map((item, index) => (
-            <li key={index} className="hover:text-gray-300">
-              <a href={item} aria-label={`Navigate to ${item}`}>{item}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <img src="logo2.svg" alt="TimesLogo" />
-      </div>
-    </div>
->>>>>>> 3b7a2b125ac876aeaf9d448a5ab4fdfc77908eba
 
   return (
     <nav className="relative">
       {/* Desktop and Mobile Main Bar */}
       <div className='flex flex-row items-center'>
         {/* Left Logo */}
-        <div className="w-24 md:w-auto">
+        <div className=" w-24 md:w-auto">
           <img src="logo1.svg" alt="TimesLogo" className="h-auto" />
         </div>
 
