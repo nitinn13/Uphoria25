9b1f9db748b416405c3553befe574e30b9dc0f0f
nitinn13 <nitinjha080@gmail.com>
2025-01-23 01:58:18 +0530

    fixed banners

--- src/pages/EventCategoryPage.jsx
+++ src/pages/EventCategoryPage.jsx
@@ -11,7 +11,6 @@ const EventCategoryPage = () => {
     fetch("/events.json")
       .then((res) => res.json())
       .then((data) => {
-
         const categoryData = data.find(
           (item) => item.category.toLowerCase() === category.toLowerCase()
         );
@@ -27,13 +26,15 @@ const EventCategoryPage = () => {
   }, [category]);
 
   return (
-
-
-    <div className=" flex flex-col items-center  text-white text-center ">
-      <h1 className="text-center text-[14vh] font-bold text-[#E6C373] font-charm 
-               [text-shadow:0_0_5px_rgba(230,195,115,0.6),0_0_10px_rgba(230,195,115,0.4)] uppercase">{categoryName}</h1>
+    <div className="flex flex-col items-center text-white text-center overflow-hidden">
+      <h1
+        className="text-center text-[14vh] font-bold text-[#E6C373] font-charm 
+               [text-shadow:0_0_5px_rgba(230,195,115,0.6),0_0_10px_rgba(230,195,115,0.4)] uppercase"
+      >
+        {categoryName}
+      </h1>
       {events.length > 0 ? (
-        <div className="grid grid-cols-2 items-center justify-center gap-8 px-5 pb-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 px-3 pb-6 w-full">
           {events.map((event) => (
             <a
               href={event.link}
@@ -42,26 +43,24 @@ const EventCategoryPage = () => {
               className="block"
             >
               <div
-                className="border-2 border-[#E6C373] rounded-lg hover:bg-[rgba(25,25,112,0.4)] transition-all duration-300 ease-in-out w-[40vw] overflow-hidden aspect-[1920/560]"
+                className="border-2 border-[#E6C373] h-[13vh] sm:h-[25vh] rounded-lg hover:bg-[rgba(25,25,112,0.4)] transition-all duration-300 ease-in-out w-full sm:w-[40vw] overflow-hidden"
               >
                 <img
                   src={event.banner}
                   alt={`${event.name} banner`}
                   loading="lazy"
-                  className="object-cover w-full h-full"
+                  className="object-contain w-full h-full"
                 />
               </div>
-
-
             </a>
           ))}
-
         </div>
       ) : (
-        <p className="text-xl text-gray-400 mt-10">No events available for this category.</p>
+        <p className="text-xl text-gray-400 mt-10">
+          No events available for this category.
+        </p>
       )}
     </div>
-
   );
 };
 
